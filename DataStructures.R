db1734119aea7e4ec8f09a96886a7adb1d5255b8
My Macbook Pro <mac@macs-MacBook-Air.local>
2018-11-16 20:26:58 +0530

    Added matrices based operations

--- DataStructures.R
+++ DataStructures.R
@@ -48,9 +48,45 @@ hist(x1)
 hist(x1, freq = F)
 lines(density(x1),col = 3)
 
-
-
 #matrix ----
+
+100:111
+length(100:111)
+m1 = matrix(100:111, 3, 4) 
+m1
+class(m1)
+attributes (m1)
+  #access elements
+m1[1] #1st element
+m1[1,] #1st row
+m1[1,2:3] #1st row, 2nd to 3rd element
+m1[c(1,3),] #leaves our 2nd row
+m1[, -c(1,3)] #leaves out 1st and 3rd column
+m1[1,c(2,3)] #1st row, 2nd and 3rd column
+m1
+
+  #changing names in matrices
+paste("C","D",sep="+")
+paste("C",1:100,sep="*")
+(colnames(m1) = paste('col', 1:4, sep='@')) #dimensions mst be same as that of table
+m1
+  #findind central tendencies of row and column data
+colSums(m1) 
+rowSums(m1)
+colMeans(m1)
+rowMeans(m1)
+
+m2 = matrix(1:25,5,5)
+m2
+cbind(m2,m2)
+rbind(m2,m2)
+m2
+  #changing values of row and column as required
+sweep(m2, MARGIN = 1, STATS = c(2,3,4,5,6), FUN = "+") #row wise
+sweep(m2, MARGIN = 2, STATS = c(2,2.2,2.4,2.6,2.8), FUN = "*") # column wise
+  #Adding a row to the table.....meand and sd added
+addmargins(m2, margin=1,sum) 
+addmargins(m2, margin=2,sd)
 
 #dataframe ----
 
